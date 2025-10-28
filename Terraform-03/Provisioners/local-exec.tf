c4fc687b97860d95483093ced8a7a91f3ceb12fe
GitEic-Bhavin <bhavsarbhavin16@gmail.com>
2025-10-28 12:36:28 +0530

    Update push

--- Terraform-03/Provisioners/local-exec.tf
+++ Terraform-03/Provisioners/local-exec.tf
@@ -1,12 +1,12 @@
-# resource "aws_instance" "life" {
-#     ami = "ami-00111452cb3c5dda0"
-#     instance_type = "t2.micro"
+resource "aws_instance" "life" {
+    ami = "ami-00111452cb3c5dda0"
+    instance_type = "t2.micro"
 
-#     provisioner "local-exec" {
-#       command = "echo ${self.private_ip} > private_IP.txt"
-#     }
-# }
+    provisioner "local-exec" {
+      command = "echo ${self.private_ip} > private_IP.txt"
+    }
+}
 
-# output "private_IP" {
-#   value = aws_instance.life.private_ip
-# }
+output "private_IP" {
+  value = aws_instance.life.private_ip
+}
